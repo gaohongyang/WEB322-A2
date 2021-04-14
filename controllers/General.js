a42e22b93265484948c715b79ed1267fd0b3d09d
gaohongyang <54781674+gaohongyang@users.noreply.github.com>
2021-04-14 10:43:07 -0400

    A4--FakeDB remvoed, everything in MongoDB now

--- controllers/General.js
+++ controllers/General.js
@@ -1,28 +1,70 @@
 const express = require('express')
 const router = express.Router();
-const fakeDB = require("../model/FakeDB")
+const productModel = require('../model/Products');
 
 router.get("/", (req, res) => {
-    res.render("General/index", {
-        title: 'Home Page',
-        featuredMovies: fakeDB.getFeaturedMovies(),
-        featuredTV: fakeDB.getFeaturedTV(),
-        deals: fakeDB.getAllDeals()
+    productModel.find({featured: true, category: "Movie"})
+    .then((featuredMovies)=>{
+        const filteredFeaturedMovies = featuredMovies.map(movie=>{
+            return{
+                index: movie.index,
+                smallPoster: movie.smallPoster
+            }
+        });
+
+        productModel.find({featured: true, category: "TV Show"})
+        .then((featuredShows)=>{
+            const filteredFeaturedShows = featuredShows.map(show=>{
+                return{
+                    index: show.index,
+                    smallPoster: show.smallPoster
+                }
+            });
+            res.render("General/index", {
+                title: 'Home Page',
+                filteredFeaturedMovies,
+                filteredFeaturedShows
+            })
+        })
+        .catch(err=>console.log(`Error: ${err}`))
     })
+    .catch(err=>console.log(`Error: ${err}`))
+
 })
 
 router.get("/products", (req, res) => {
-    res.render("General/products", {
-        title: 'Movies || TV Shows',
-        products: fakeDB.getAllProducts()
+    productModel.find()
+    .then((products)=>{
+        const allProducts = products.map(product=>{
+            return{
+                title: product.title,
+                index: product.index,
+                smallPoster: product.smallPoster
+            }
+        });
+        res.render("General/products", {
+            title: "Movies || TV Shows",
+            products: allProducts
+        })
     })
+    .catch(err=>console.log(`Error: ${err}`))
 })
 
 router.get('/products/:id', (req, res) => {
-    res.render("General/productDescription", {
-        title: 'test',
-        product: fakeDB.getAProduct(req.params.id)
+    productModel.findOne({index:req.params.id})
+    .then((product)=>{
+        const{largePoster, smallPoster, title, description, sellPrice, rentPrice} = product;
+        res.render("General/productDescription", {
+            title: "Description",
+            productTitle: title,
+            largePoster,
+            smallPoster,
+            description,
+            sellPrice,
+            rentPrice
+        })
     })
+    .catch(err=>console.log(`Error: ${err}`))
 })
 
 module.exports=router;
